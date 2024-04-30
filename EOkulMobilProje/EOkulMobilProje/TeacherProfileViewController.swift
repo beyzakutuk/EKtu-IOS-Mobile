1e223a698d2d4b3fdf420913014f3176fe56b7a5
Beyza Kütük <beyzakutuk55@gmail.com>
2024-04-30 20:58:45 +0300

    Teacher Login View Controller & Connect views

---
+++ EOkulMobilProje/EOkulMobilProje/TeacherProfileViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TeacherProfileViewController.swift
+//  EOkulMobilProje
+//
+//  Created by Beyza Kütük on 30.04.2024.
+//
+
+import UIKit
+
+class TeacherProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
