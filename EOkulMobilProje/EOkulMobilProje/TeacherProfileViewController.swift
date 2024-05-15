90017143c035d47b170d09a4a282a6bb2ea23b0d
Beyza Kütük <beyzakutuk55@gmail.com>
2024-05-15 22:35:56 +0300

    Logout button link | TeacherProfileViewController

--- EOkulMobilProje/EOkulMobilProje/TeacherProfileViewController.swift
+++ EOkulMobilProje/EOkulMobilProje/TeacherProfileViewController.swift
@@ -16,14 +16,20 @@ class TeacherProfileViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func exitButtonClicked(_ sender: UIButton) {
+        
+        let exitAlert = UIAlertController(title: "Çıkış Yap", message: "Çıkış yapmak istediğinizden emin misiniz?", preferredStyle: .alert)
+        
+        //evet
+        exitAlert.addAction(UIAlertAction(title: "Evet", style: .default , handler: {(_) in
+                                          // Giriş sayfasına geri dön
+            self.performSegue(withIdentifier: "backToLogin", sender: self)
+                                      }))
+        //iptal
+        exitAlert.addAction(UIAlertAction(title: "İptal", style: .cancel , handler: nil))
+        
+        self.present(exitAlert, animated: true, completion: nil)
     }
-    */
+    
 
 }
