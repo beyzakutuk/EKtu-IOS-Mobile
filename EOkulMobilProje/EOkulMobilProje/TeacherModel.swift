e0b03958ffe9c100a5beceb81ab625b21d39a9ee
Beyza Kütük <beyzakutuk55@gmail.com>
2024-05-19 01:14:21 +0300

    Teacher Model

---
+++ EOkulMobilProje/EOkulMobilProje/TeacherModel.swift
@@ -0,0 +1,15 @@
+//
+//  TeacherModel.swift
+//  EOkulMobilProje
+//
+//  Created by Beyza Kütük on 19.05.2024.
+//
+
+import Foundation
+
+class TeacherModel : UserModel
+{
+    override init(isim: String, soyisim: String, tcKimlikNo: String, sifre: String) {
+            super.init(isim: isim, soyisim: soyisim, tcKimlikNo: tcKimlikNo, sifre: sifre)
+        }
+}
