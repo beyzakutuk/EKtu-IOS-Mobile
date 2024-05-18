b72a7fe3a81309586c1269dd51c5817f7c9fb68d
Beyza Kütük <beyzakutuk55@gmail.com>
2024-05-19 02:38:55 +0300

    Logout button link | DirectorProfileViewController

--- EOkulMobilProje/EOkulMobilProje/DirectorProfileViewController.swift
+++ EOkulMobilProje/EOkulMobilProje/DirectorProfileViewController.swift
@@ -18,7 +18,20 @@ class DirectorProfileViewController: UIViewController {
         setupProfile()
     }
     
-
+    @IBAction func exitButtonClicked(_ sender: UIButton) {
+        
+        let exitAlert = UIAlertController(title: "Çıkış Yap", message: "Çıkış yapmak istediğinizden emin misiniz?", preferredStyle: .alert)
+        
+        //evet
+        exitAlert.addAction(UIAlertAction(title: "Evet", style: .default , handler: {(_) in
+                                          // Giriş sayfasına geri dön
+            self.performSegue(withIdentifier: "backToLogin", sender: self)
+                                      }))
+        //iptal
+        exitAlert.addAction(UIAlertAction(title: "İptal", style: .cancel , handler: nil))
+        self.present(exitAlert, animated: true, completion: nil)
+    }
+    
     private func setupProfile() {
             guard let director = director else { return }
             nameLabel.text = "\(director.isim) \(director.soyisim)"
