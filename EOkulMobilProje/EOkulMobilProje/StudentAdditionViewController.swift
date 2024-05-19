a74594ee53987d4bce1a796c3072deb6412f2175
Beyza Kütük <beyzakutuk55@gmail.com>
2024-05-19 03:27:40 +0300

    modification controller - StudentAdditionVC

---
+++ EOkulMobilProje/EOkulMobilProje/StudentAdditionViewController.swift
@@ -0,0 +1,53 @@
+//
+//  StudentAdditionViewController.swift
+//  EOkulMobilProje
+//
+//  Created by Beyza Kütük on 19.05.2024.
+//
+
+import UIKit
+
+class StudentAdditionViewController: UIViewController {
+    
+    @IBOutlet weak var tcKimlikField: UITextField!
+    @IBOutlet weak var isimField: UITextField!
+    @IBOutlet weak var soyisimField: UITextField!
+    @IBOutlet weak var sinifField: UITextField!
+    @IBOutlet weak var sifreField: UITextField!
+    
+    @IBOutlet weak var submitButton: UIButton!
+    
+    @IBOutlet weak var onayLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupViews()
+    }
+    
+    // MARK: - Private Methods
+        private func setupViews() {
+            // Text fieldlar için editingChanged eventini dinleyerek validateFields fonksiyonunu çağır
+            [isimField, soyisimField, tcKimlikField, sifreField, sinifField].forEach { textField in
+                textField?.addTarget(self, action: #selector(validateFields), for: .editingChanged)
+            }
+            
+            validateFields() // Başlangıçta butonu pasif hale getirmek için
+        }
+    
+    @objc private func validateFields()
+    {
+        // Bütün text fieldların dolu olup olmadığını kontrol et
+        let isFormValid = !(isimField.text?.isEmpty ?? true) &&
+                          !(soyisimField.text?.isEmpty ?? true) &&
+                          !(tcKimlikField.text?.isEmpty ?? true) &&
+                          !(sifreField.text?.isEmpty ?? true) &&
+                          !(sinifField.text?.isEmpty ?? true)
+                
+        submitButton.isEnabled = isFormValid
+    }
+    
+
+   
+
+}
