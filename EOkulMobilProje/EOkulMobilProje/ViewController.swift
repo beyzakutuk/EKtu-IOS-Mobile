3ac249603712cc013d853f35914e9f944ffff2f4
Beyza Kütük <beyzakutuk55@gmail.com>
2024-05-24 21:42:08 +0300

    feat: refresh token control for students

--- EOkulMobilProje/EOkulMobilProje/ViewController.swift
+++ EOkulMobilProje/EOkulMobilProje/ViewController.swift
@@ -18,6 +18,38 @@ class ViewController: UIViewController {
         self.view.endEditing(true)
     }
 
+    @IBAction func StudentButtonClicked(_ sender: Any) {
+        guard let storyboard = self.storyboard else {
+            fatalError("Storyboard bulunamadı.")
+        }
+        
+        
+        var myRefresh:Any?
+        
+        myRefresh = UserDefaults.standard.string(forKey: "refreshToken")
+        
+        if myRefresh != nil{
+            
+            guard let studentProfileVC = storyboard.instantiateViewController(withIdentifier: "StudentProfileVC") as? StudentProfileViewController else {
+                fatalError("StudentProfileViewController bulunamadı veya uygun tipte değil.")
+            }
+            
+            studentProfileVC.modalPresentationStyle = .fullScreen
+            self.present(studentProfileVC, animated: true, completion: nil)
+            
+        }
+        else
+        {
+            guard let studentLoginVC = storyboard.instantiateViewController(withIdentifier: "StudentLoginVC") as? StudentLoginViewController else {
+                fatalError("StudentLoginViewController bulunamadı veya uygun tipte değil.")
+            }
+            
+            studentLoginVC.modalPresentationStyle = .fullScreen
+            self.present(studentLoginVC, animated: true, completion: nil)
+            
+        }
+    }
 
+    
 }
 
