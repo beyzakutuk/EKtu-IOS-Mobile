8a140bd6234a4ad231dd2137c996615f91d757b4
Beyza Kütük <beyzakutuk55@gmail.com>
2024-05-24 23:07:28 +0300

    feat: refresh token control for directors

--- EOkulMobilProje/EOkulMobilProje/ViewController.swift
+++ EOkulMobilProje/EOkulMobilProje/ViewController.swift
@@ -83,7 +83,39 @@ class ViewController: UIViewController {
         }
     }
     
-
+    @IBAction func DirectorButtonClicked(_ sender: Any) {
+        
+        guard let storyboard = self.storyboard else {
+            fatalError("Storyboard bulunamadı.")
+        }
+        
+        
+        var myRefresh:Any?
+        
+        myRefresh = UserDefaults.standard.string(forKey: "refreshTokenPrincipal")
+        
+        if myRefresh != nil{
+            
+            guard let DirectorProfileVC = storyboard.instantiateViewController(withIdentifier: "DirectorProfileVC") as? DirectorProfileViewController else {
+                fatalError("DirectorProfileViewController bulunamadı veya uygun tipte değil.")
+            }
+            
+            DirectorProfileVC.modalPresentationStyle = .fullScreen
+            self.present(DirectorProfileVC, animated: true, completion: nil)
+            
+        }
+        else
+        {
+            guard let DirectorLoginVC = storyboard.instantiateViewController(withIdentifier: "DirectorLoginVC") as? DirectorLoginViewController else {
+                fatalError("DirectorLoginViewController bulunamadı veya uygun tipte değil.")
+            }
+            
+            DirectorLoginVC.modalPresentationStyle = .fullScreen
+            self.present(DirectorLoginVC, animated: true, completion: nil)
+            
+        }
+    }
+    
     
 }
 
