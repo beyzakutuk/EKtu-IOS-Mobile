149a9a3d060aa3ca7a9fda43d393cc67df011ac8
Beyza Kütük <beyzakutuk55@gmail.com>
2024-05-24 21:41:46 +0300

    fixed:API errors fixed for student

--- EOkulMobilProje/EOkulMobilProje/StudentLoginViewController.swift
+++ EOkulMobilProje/EOkulMobilProje/StudentLoginViewController.swift
@@ -6,7 +6,7 @@
 //
 
 import UIKit
-
+import Alamofire
 
 class StudentLoginViewController: UIViewController {
     
@@ -16,9 +16,8 @@ class StudentLoginViewController: UIViewController {
     @IBOutlet weak var passwordField: UITextField!
     @IBOutlet weak var loginButton: UIButton!
     
-    var loggedInStudent: StudentModel?
     // MARK: -FUNCTIONS
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setInitViews()
@@ -39,104 +38,96 @@ class StudentLoginViewController: UIViewController {
     {
         loginButton.isEnabled = usernameField.text != "" && passwordField.text != ""
     }
-
+    
+    
     @IBAction func loginButtonClk(_ sender: Any) {
         
-        var myValue:String?
-        myValue = UserDefaults.standard.string(forKey: "refreshToken")
-        
-        if  myValue == nil
-        {
-            getToken()
+        guard let username = usernameField.text,
+              let password = passwordField.text else {
+            
+            return
         }
         
-
-        /*
-        let tckn = usernameField.text ?? ""
-        let sifre = passwordField.text ?? ""
-
-        if let student = StudentDatabase.studentDatabase[tckn], student.sifre == sifre
-        {
-            loggedInStudent = student
-
-            performSegue(withIdentifier: "toStudentProfilePage", sender: nil)
-            print("Giriş başarılı, profil sayfasına yönlendiriliyor...")
+        guard let storyboard = self.storyboard else {
+            fatalError("Storyboard bulunamadı.")
         }
-        else
-        {
-            // Giriş başarısız, hata mesajı göster
-            let alert = UIAlertController(title: "Hata", message: "Hatalı TC kimlik numarası veya şifre!", preferredStyle: .alert)
-            alert.addAction(UIAlertAction(title: "Tamam", style: .default, handler: nil))
-            present(alert, animated: true, completion: nil)
-            print("Hatalı TC kimlik numarası veya şifre!")
-        }*/
+        
+        
+        let apiUrl = URL(string: "https://localhost:7134/connect/token")!
+        
+        var request = URLRequest(url: apiUrl)
+        request.httpMethod = "POST"
+        request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+        
+        let authString = "\("ResourceOwnerStudent"):\("secret")"
+        let authData = authString.data(using: .utf8)!
+        let authHeader = "Basic \(authData.base64EncodedString())"
+        request.setValue(authHeader, forHTTPHeaderField: "Authorization")
+        
+        let bodyParameters = [
+            "grant_type": "password",
+            "username": username,
+            "password": password
+        ]
+        
+        request.httpBody = bodyParameters
+            .map { "\($0.key)=\($0.value)" }
+            .joined(separator: "&")
+            .data(using: .utf8)
+        
+        let session = URLSession(configuration: .default, delegate: MySessionDelegate(), delegateQueue: nil)
+        let task = session.dataTask(with: request) { data, response, error in
+            
+            guard let httpResponse = response as? HTTPURLResponse else {
+                print("Geçersiz HTTP yanıtı")
+                return
+            }
+            
+            print("Status code: \(httpResponse.statusCode)")
+            
+            if httpResponse.statusCode == 200
+            {
+                do {
+                    if let json = try JSONSerialization.jsonObject(with: data!, options: []) as? [String: Any] {
+                        print("Cevap JSON: \(json)")
+                        if let refreshToken = json["refresh_token"] as? String {
+                            self.saveTokens(refreshToken: refreshToken)
+                        }
+                    }
+                }
+                catch
+                {
+                    print("JSON dönüşüm hatası: \(error.localizedDescription)")
+                }
+                DispatchQueue.main.async {
+                    
+                    
+                    guard let studentProfileVC = storyboard.instantiateViewController(withIdentifier: "StudentProfileVC") as? StudentProfileViewController else {
+                        fatalError("StudentProfileViewController bulunamadı veya uygun tipte değil.")
+                    }
+                    
+                    
+                    studentProfileVC.modalPresentationStyle = .fullScreen
+                    self.present(studentProfileVC, animated: true, completion: nil)
+                 }
+            }
+            else
+            {
+                DispatchQueue.main.async {
+                    let alert = UIAlertController(title: "Hata", message: "Kullanıcı adı veya şifre hatalı", preferredStyle: .alert)
+                    alert.addAction(UIAlertAction(title: "Tamam", style: .default, handler: nil))
+                    self.present(alert, animated: true, completion: nil)
+                }
+            }
+
+        }
+        task.resume()
     }
     
     func saveTokens(refreshToken:String){
         UserDefaults.standard.set(refreshToken , forKey: "refreshToken")
     }
-    
-    func getToken() {
-        let apiUrl = URL(string: "https://localhost:7134/connect/token")!
-
-        // İstek parametreleri
-        guard let username = usernameField.text, let password = passwordField.text else {
-            print("Kullanıcı adı veya şifre eksik")
-            return
-        }
-
-    
-
-        // İstek oluşturma
-        var request = URLRequest(url: apiUrl)
-        request.httpMethod = "POST"
-        request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
-
-        let authString = "\("ResourceOwnerStudent"):\("secret")"
-            let authData = authString.data(using: .utf8)!
-            let authHeader = "Basic \(authData.base64EncodedString())"
-            request.setValue(authHeader, forHTTPHeaderField: "Authorization")
-
-        let bodyParameters = [
-                "grant_type": "password",
-                "username": username,
-                "password": password
-            ]
-            
-            request.httpBody = bodyParameters
-                .map { "\($0.key)=\($0.value)" }
-                .joined(separator: "&")
-                .data(using: .utf8)
-        let session = URLSession(configuration: .default, delegate: MySessionDelegate(), delegateQueue: nil)
-        let task = session.dataTask(with: request) { data, response, error in
-            if let error = error?.localizedDescription {
-                print("Hata: \(error)")
-                return
-            }
-
-            // Cevap alındığında
-            if let httpResponse = response as? HTTPURLResponse {
-                print("Status code: \(httpResponse.statusCode)")
-            }
-
-            // Cevap verisi kontrolü
-            if let data = data {
-                if let json = try? JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
-                    print("Cevap JSON: \(json)")
-                    if let refreshToken = json["refresh_token"] as? String {
-                           print(refreshToken)
-                        self.saveTokens(refreshToken: refreshToken)
-                       }
-                       
-                } else {
-                    print("Geçersiz cevap")
-                }
-            }
-        }
-        task.resume()
-    }
-
-    
+   
     @IBAction func forgotPasswordButton(_ sender: UIButton) {
         
         // "Şifreni mi unuttun?" butonuna tıklandığında eylem
@@ -159,18 +150,8 @@ class StudentLoginViewController: UIViewController {
         
         present(alertController, animated: true, completion: nil)
         
-        }
-    
-    // MARK: - Navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "toStudentProfilePage" {
-            if let destinationVC = segue.destination as? StudentProfileViewController {
-                destinationVC.student = loggedInStudent
-            }
-        }
     }
-
-
+    
 }
 
 
