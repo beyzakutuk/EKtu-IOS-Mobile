0dfeeb73dff0567e6e927570609390a80a0dcf93
Beyza Kütük <beyzakutuk55@gmail.com>
2024-05-27 20:26:31 +0300

    feat: Add function to fetch Teacher data

--- EOkulMobilProje/EOkulMobilProje/TeacherProfileViewController.swift
+++ EOkulMobilProje/EOkulMobilProje/TeacherProfileViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class TeacherProfileViewController: UIViewController {
+class TeacherProfileViewController: UIViewController , URLSessionDelegate {
 
     @IBOutlet weak var nameLabel: UILabel!
     
@@ -17,6 +17,61 @@ class TeacherProfileViewController: UIViewController {
         super.viewDidLoad()
 
        setupProfile()
+       GetInformation()
+    }
+    
+    func urlSession(_ session: URLSession, didReceive challenge: URLAuthenticationChallenge, completionHandler: @escaping (URLSession.AuthChallengeDisposition, URLCredential?) -> Void) {
+            completionHandler(.useCredential, URLCredential(trust: challenge.protectionSpace.serverTrust!))
+        }
+    
+    func GetInformation(){
+        
+        
+        guard let token = UserDefaults.standard.string(forKey: "refreshTokenTeacher") else {
+                   print("Token yok veya geçersiz")
+                   return
+               }
+        guard let url = URL(string: "https://localhost:7134/connect/userinfo") else {
+                print("Geçersiz URL")
+                return
+            }
+        var request = URLRequest(url: url)
+            request.httpMethod = "GET"
+        
+        
+        request.setValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
+        
+        let session = URLSession(configuration: .default, delegate: self, delegateQueue: nil)
+        let task = session.dataTask(with: request) { data, response, error in
+            print("Request tamamlandı")
+                
+            if let error = error {
+                print("Hata: \(error.localizedDescription)")
+                return
+            }
+                    
+            guard let data = data else {
+                print("Boş veri")
+                return
+            }
+                    
+            // Response'u parse edin ve kullanın
+            do {
+                if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
+                    print("Kullanıcı Bilgisi: \(json["name"]!)")
+                    print("Kullanıcı id: \(json["sub"]!)")
+                            
+                } else {
+                            
+                        }
+                
+            } catch {
+                        print("JSON parse hatası: \(error.localizedDescription)")
+                }
+        }
+                
+        task.resume()
+
     }
     
 
