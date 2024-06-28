82175abb571c4ae69fdea3b60c2179cac28ed7ad
Beyza Kütük <beyzakutuk55@gmail.com>
2024-06-28 18:18:54 +0300

    feat: send POST request for letter grade calculation via API

--- EOkulMobilProje/EOkulMobilProje/ApproveLessonsViewController.swift
+++ EOkulMobilProje/EOkulMobilProje/ApproveLessonsViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class ApproveLessonsViewController: UIViewController , URLSessionDelegate{
 
+    @IBOutlet weak var Approve: UIButton!
+    @IBOutlet weak var Calculate: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -69,8 +72,71 @@ class ApproveLessonsViewController: UIViewController , URLSessionDelegate{
         }
         
         task.resume()
+        
+        Approve.isEnabled = false
+        
     }
     
+    @IBAction func letterGradeCalculate(_ sender: Any) {
+        
+        guard let token = UserDefaults.standard.string(forKey: "refreshTokenPrincipal") else {
+                   print("Token yok veya geçersiz")
+                   return
+               }
+        guard let url = URL(string: "https://localhost:7253/api/principal/AllStudentCalculateLetterGrade") else {
+                print("Geçersiz URL")
+                return
+            }
+        var request = URLRequest(url: url)
+            request.httpMethod = "GET"
+        
+        request.setValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
+        
+        let session = URLSession(configuration: .default, delegate: MySessionDelegate(), delegateQueue: nil)
+        let task = session.dataTask(with: request) { data, response, error in
+            
+            if let error = error {
+                print("İstek hatası: \(error.localizedDescription)")
+                return
+            }
+            
+            guard let httpResponse = response as? HTTPURLResponse else {
+                print("Geçersiz yanıt")
+                return
+            }
+            
+            print("HTTP Durum Kodu: \(httpResponse.statusCode)")
+            
+            guard let data = data else {
+                print("Veri yok")
+                return
+            }
+            
+            // Gelen veriyi yazdırın
+            if let responseString = String(data: data, encoding: .utf8) {
+                print("Yanıt Verisi: \(responseString)")
+            }
+            
+            do {
+                // Yanıtı işleyin
+                if let jsonResponse = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
+                    print("Yanıt: \(jsonResponse)")
+                    
+                }
+                    else {
+                    print("JSON formatı beklenmiyor")
+                }
+            } catch let error {
+                print("JSON Parsing error: \(error.localizedDescription)")
+            }
+            
+        }
+        
+        task.resume()
+        
+        Calculate.isEnabled = false
+        
+    }
     
     
 }
